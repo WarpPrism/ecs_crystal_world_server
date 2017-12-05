4fd8f781278761cfbf1060512bce716c1f600992
ZhouJihao <249662605@qq.com>
2017-12-05 18:48:31 +0800

    [add] 404 page

--- server.js
+++ server.js
@@ -28,41 +28,26 @@ app.use(router);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(favicon(__dirname + '/static/favicon.ico'));
 
-
-// app.use(favicon(__dirname + '/static/favicon.ico'));
-
-/// catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-/// error handlers
-
-// development error handler
-// will print stacktrace
-
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
-    });
-}
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
+    if (err.status == 404) {
+        res.render('404', {
+            title: '页面不见了 | 404'
+        });
+    } else {
+        
+    }
 });
 
 app.set('port', process.env.PORT || 3000);
